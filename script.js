984e80a5663d599ff7deccb94c15bd6640941232
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-03-04 21:44:00 +0530

    All done just npm package remains

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let mainContainer=document.querySelector(".container")
 let submitbutton = document.querySelector(".submit");
 let userNameInput = document.querySelector("#name");
+let linkedinInput = document.querySelector("#linkedinusername");
+let instragramInput = document.querySelector("#instagramname");
+const mediaQuery = window.matchMedia("(max-width: 550px)")
+
 // let ageInput = document.querySelector("#Age");
 // let dobInput = document.querySelector("#DOB");
 let gmailInput = document.querySelector("#Github");
@@ -9,32 +13,46 @@ let allData;
 const baseURL = "https://api.github.com/users/";
 document.addEventListener("DOMContentLoaded", () => {
   submitbutton.addEventListener("click", async function call(e) {
-    mainContainer.innerHTML=""
     e.preventDefault();
     let userName = userNameInput.value;
+    console.log(userName);
     // let age = ageInput.value;
     // let dob = dobInput.value;
     let gmail = gmailInput.value;
+    let linkedinUsername=linkedinInput.value
+    let instragramUsername=instragramInput.value
     let mainURL = `${baseURL}${userName}`;
     console.log(mainURL);
     try {
       const response = await fetch(mainURL);
-       allData = await response.json();
+      allData = await response.json();
     } catch (error) {
       console.log(error);
     }
-    let gitHubUserId=allData.id
-    console.log(gitHubUserId);
+    let githubUsername=allData.login
     let gitHubBio=allData.bio
     console.log(gitHubBio);
     let gitHubName=allData.name
     console.log(gitHubName);
-    let gitHubFollowers=allData.followers
+    if (gitHubName===undefined) {
+        mainContainer.innerHTML="<h1>Please Enter a Valid UserName</h1>"
+    }else{
+      mainContainer.innerHTML=""
+      let gitHubFollowers=allData.followers
     console.log(gitHubFollowers);
     let gitHubFollowing=allData.following
     console.log(gitHubFollowing)
     let gitHubPics=allData.avatar_url
     console.log(gitHubPics);
+    let githubcreateddate=new Date(allData.created_at)
+    let githubcreatetime= githubcreateddate.toLocaleTimeString()
+    let githubMainDate=githubcreateddate.toLocaleDateString("en-IN",{
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+    })
+    console.log(githubcreateddate);
+    console.log(githubMainDate);
     let htmlURL=allData.html_url
     const div1=document.createElement("div")
     div1.classList.add("smaincontainer")
@@ -42,6 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const div2=document.createElement("div")
     div2.classList.add("cardcontainer")
     div1.appendChild(div2)
+    div2.style.backgroundColor="#FFFFFF"
     const imgContainer=document.createElement("div")
     imgContainer.classList.add("imgContainer")
     div2.appendChild(imgContainer)
@@ -57,17 +76,28 @@ document.addEventListener("DOMContentLoaded", () => {
     div2.appendChild(nameContainer)
     const nameh1=document.createElement("h1")
     nameh1.classList.add("nameh1")
-    nameh1.innerText=`${gitHubName}`
-    nameContainer.appendChild(nameh1)
+    nameh1.style.fontSize="28px"
+    if (gitHubName===null) {
+        nameh1.innerText=`${githubUsername}`
+        nameContainer.appendChild(nameh1)
+        console.log("here2");
+    }else{
+      nameh1.innerText=`${gitHubName}`
+      nameContainer.appendChild(nameh1)
+    }
     const bioContainer=document.createElement("div")
     bioContainer.classList.add("bioContainer")
     div2.appendChild(bioContainer)
-    const biop=document.createElement("textarea")
-    biop.readOnly="true"
-    biop.style.resize="none";
-    biop.maxLength="156"
+    const biop=document.createElement("p")
     biop.classList.add("biop")
-    biop.innerText=`${gitHubBio}`
+    if (gitHubBio===null) {
+      console.log("nana");
+       biop.innerText=`You Dont Have A Bio Sir,Kindly Update Your Bio At Your GitHub Profile`
+    }else{
+       biop.innerText=`${gitHubBio}`
+    }
+    biop.style.textAlign = "justify"
+    biop.style.overFlow="hidden"
     bioContainer.appendChild(biop)
     const follower_followingcontainer=document.createElement("div")
     follower_followingcontainer.classList.add("follower_followingcontainer")
@@ -105,8 +135,62 @@ document.addEventListener("DOMContentLoaded", () => {
     const githubsvg=document.createElement("img")
     githubsvg.setAttribute("src","icons8-github.svg")
     linkdinsvgContainer.appendChild(linkdinsvg)
+    const linkedinh1=document.createElement("h1")
+    linkedinh1.classList.add("githubh1")
+    linkedinh1.innerText=`${linkedinUsername}`
+    linkdinsvgContainer.appendChild(linkedinh1)
     instagramsvgContainer.appendChild(instagramsvg)
+    const instagramh1=document.createElement("h1")
+    instagramh1.classList.add("githubh1")
+    instagramh1.innerText=`${instragramUsername}`
+    instagramsvgContainer.appendChild(instagramh1)
     githubsvgContainer.appendChild(githubsvg)
+    const githubh1=document.createElement("h1")
+    githubh1.classList.add("githubh1")
+    githubh1.innerText=`${userName}`
+    githubsvgContainer.appendChild(githubh1)
+    const emailContainer=document.createElement("div")
+    emailContainer.classList.add("emailContainer")
+    div2.appendChild(emailContainer)
+    const svgemailcontainer=document.createElement("div")
+    svgemailcontainer.classList.add("svgemailcontainer")
+    emailContainer.appendChild(svgemailcontainer)
+    const svgemail=document.createElement("img")
+    svgemail.setAttribute("src","icons8-gmail.svg")
+    svgemail.classList.add("svgemail")
+    svgemailcontainer.appendChild(svgemail)
+    const emailh1=document.createElement("h1")
+    emailh1.innerText=`${gmail}`
+    const emailh1Container=document.createElement("div")
+    emailContainer.appendChild(emailh1Container)
+    emailh1.classList.add("emailh1")
+    emailh1Container.appendChild(emailh1)
+    const ageContainer=document.createElement("div")
+    ageContainer.classList.add("ageContainer")
+    div2.appendChild(ageContainer)
+    const ageSvgContainer=document.createElement("div")
+    ageSvgContainer.classList.add("ageSvgContainer")
+    ageContainer.appendChild(ageSvgContainer)
+    const svgAge=document.createElement("img")
+    svgAge.setAttribute("src","icons8-calculator.svg")
+    svgAge.classList.add("svgAge")
+    ageSvgContainer.appendChild(svgAge)
+    const ageh1=document.createElement("h1")
+    ageh1.classList.add("ageh1")
+    ageh1.innerText=`GitHub Creation Date:${githubMainDate}`
+    ageSvgContainer.appendChild(ageh1)
+    const svgTimeContainer=document.createElement("div")
+    svgTimeContainer.classList.add("svgTimeContainer")
+    div2.appendChild(svgTimeContainer)
+    const svgTime=document.createElement("img")
+    svgTime.setAttribute("src","icons8-clock.svg")
+    svgTime.classList.add("svgTime")
+    svgTimeContainer.appendChild(svgTime)
+    const svgh1=document.createElement("h1")
+    svgh1.classList.add("svgh1")
+    svgh1.innerText=`CreationTime:${githubcreatetime}`
+    svgTimeContainer.appendChild(svgh1)
+    }
   });
 });
 
